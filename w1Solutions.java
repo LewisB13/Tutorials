cfe4f052d1200590139594ce797672b6760b8265
LewisB13 <lewisb13@protonmail.com>
2026-05-20 05:04:40 +0100

    Add files via upload

---
+++ w1Solutions.java
@@ -0,0 +1,41 @@
+public class w1Solutions{
+
+   public static void main (String[] args){
+   
+   //full name
+   System.out.println("Leewis Barrett");
+   //fav game, movie and food
+   System.out.println("Bully");
+   System.out.println("Smokey and the Bandit");
+   System.out.println("Pasta");
+   
+   //task 3 mutiple print statement
+   System.out.println("HELLO");
+   System.out.println("Welcome");
+   System.out.println("To java");
+   
+   // task 5 edit output
+   System.out.println("Java is fun!");
+   System.out.println("Java is annoying!");
+   
+   //task 6
+// public class w1Solutions{
+// 
+//    public static void main (String[] args){
+//CODE HERE
+// }
+// }
+
+//task 10
+
+System.out.println("WHYYYYY");
+System.out.println("why you do this");
+System.out.println("java is annoyign");
+System.out.println("this is not full solution ");
+System.out.println("just test solution");
+
+
+
+}
+
+}
