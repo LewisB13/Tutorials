b7eae68217afc2656e0349404a6c59a1578eb32a
LewisB13 <lewisb13@protonmail.com>
2026-05-20 05:13:35 +0100

    edited l;ayout

---
+++ Java/Tutorial 2/VariablesExamples.java
@@ -0,0 +1,36 @@
+public class VariablesExamples{
+
+   public static void main(String[] args){
+      //Int variable
+      int age = 31;
+      System.out.println(age);
+      
+      //Decimal value
+      double price = 9.99;
+      System.out.println(price);
+      price = 12.50;
+      System.out.println(price);
+      
+      
+      //String Variable
+      String name = "Lewis";
+      System.out.println(name);
+      
+      System.out.println(" ");
+      System.out.println("Name: " + name);
+      System.out.println("Age: " + age);
+      System.out.println("The price of monster is now: " + price);
+
+   
+   
+   
+   }
+
+
+
+
+
+
+
+
+}
